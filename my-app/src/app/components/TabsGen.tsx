9aa588d348c9def730dc8f6c548c2715772df0be
Anandydvv <anandinfo9899@gmail.com>
2025-08-23 23:39:28 +1000

    commit

---
+++ my-app/src/app/components/TabsGen.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function TabsGen() {
+  return <div style={{padding:12, border:"1px solid #888"}}>MINI TABS COMPONENT</div>;
+}
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,10 @@
-// src/app/page.tsx
-import dynamic from "next/dynamic";
-const TabsGenerator = dynamic(() => import("./components/TabsGenerator"), { ssr: false });
+import TabsGen from "./components/TabsGen";
 
 export default function Home() {
   return (
     <div style={{padding:16}}>
       <h1>Tabs</h1>
-      <TabsGenerator />
+      TabsGen 
     </div>
   );
 }
